046a5ce3720967a9272de723f687f974ac1b6c23
Costin Manolache <costin@apache.org>
2003-01-21 00:31:05 +0000

    Another round of refactoring ( no 1.0 API should be touched ).
    The "source" of descriptor data is now a sort of mbean ( or ant task :-),
    various properties are set and an execute operation is invoked. That is
    a bit more consistent with the spirit of jmx :-)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/modeler/trunk@139138 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/modeler/modules/ModelerSource.java
@@ -0,0 +1,16 @@
+package org.apache.commons.modeler.modules;
+
+import org.apache.commons.modeler.Registry;
+
+/** Source for descriptor data. More sources can be added.
+ *
+ */
+public class ModelerSource {
+
+    public void loadDescriptors( Registry registry, String location,
+                                 String type, Object source)
+            throws Exception
+    {
+        // TODO
+    }
+}
